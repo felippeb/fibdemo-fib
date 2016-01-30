cd2eacf97639536d66e5560b2db0d2688264fc6d
felippe burk <felippe.burk@emc.com>
2016-01-29 19:34:03 -0700

    updating tests

--- fibgen/tests/fibgen_tests.py
+++ fibgen/tests/fibgen_tests.py
@@ -28,4 +28,4 @@ def test_num_10_list():
 def test_nums_correct_to_25():
     fibout = fibgen.gen(25)
     fibseq = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025]
-    assert cmp(fibout, fibgen)
+    assert cmp(fibout, fibseq) == 0
